c64c9b0dae7ca579c3d3567f0a52f5647b7c6864
Shivant Sahu <shivantsahu72@gmail.com>
2026-06-29 00:21:40 +0530

    commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,15 @@
 // this file containss all the basic configuration logif for tha app server to work 
 
+import dotenv from 'dotenv'
+type serverConfig = {
+    PORT : number
+}
+
+function Loadenv() {
+    dotenv.config();
+}
+
+export const serverConfig : serverConfig = {
+    PORT : Number(process.env.PORT)|| 3000
+} 
+export default Loadenv;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import dotenv from"dotenv";
+import {Loadenv, serverConfig} from "./config";
 dotenv.config();
  
 
